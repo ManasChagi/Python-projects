b4bcebe5c41c68311786f759e4018d5a57be6193
Manas Chagi <115474765+ManasChagi@users.noreply.github.com>
2024-02-10 22:32:17 +0530

    Update scoreboard.py
    updated such that the highest score is saved, even after closing and opening back the game.

--- Snake Game/scoreboard.py
+++ Snake Game/scoreboard.py
@@ -1,25 +1,30 @@
 from turtle import Turtle
 FONT = ('Courier', 20, 'normal')
 ALIGNMENT = 'center'
-
 class Score(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt") as d:
+            self.highscore = int(d.read())
         self.penup()
         self.goto(0, 265)
         self.color("white")
-        self.write(f"Score = {self.score}", align=ALIGNMENT, font=FONT)
         self.hideturtle()
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score = {self.score} High Score = {self.highscore}", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
-        self.clear()
-        self.write(f"Score = {self.score}", align=ALIGNMENT, font=FONT)
+        self.update_scoreboard()
 
-    def game_over(self):
-        self.goto(0,0)
-        self.write("Game Over!", align= ALIGNMENT, font=('Courier', 22, 'normal'))
-
-
-
+    def reset(self):
+        if self.score > self.highscore:
+            self.highscore = self.score
+            with open("data.txt", mode="w") as d:
+                d.write(f"{self.highscore}")
+        self.score = 0
+        self.update_scoreboard()
