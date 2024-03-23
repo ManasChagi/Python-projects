aaf839ac83de3c6a8200bd603afafd95e7b439ba
Manas Chagi <115474765+ManasChagi@users.noreply.github.com>
2024-03-24 01:26:35 +0530

    Update main.py
    Added KeyError exception just to improve the code quality and ensuring that it doesn't crash on wrong key value.

--- NATO Alphabet - Phonetic  Conversion/main.py
+++ NATO Alphabet - Phonetic  Conversion/main.py
@@ -3,8 +3,22 @@ df = pandas.read_csv("nato_phonetic_alphabet.csv")
 print("Welcome to Alphabet - Phonetic conversion")
 
 phonetic_dict = { row.letter : row.code for (index, row) in df.iterrows()}
-user_input = input("Enter a word: ").upper()
-list1 = [*user_input]
+
+#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+def generate_phonetic():
+    try:
+        user_input = input("Enter a word: ").upper()
+        list1 = [*user_input]
+        output_list = [phonetic_dict[letter] for letter in list1]
+    except KeyError:
+        print("Sorry only letters in the word please.")
+        generate_phonetic()
+    else:
+        print(output_list)
+
+generate_phonetic()
+
+
 # Method- 1
 # new =[]
 # for each in list1:
@@ -14,5 +28,5 @@ list1 = [*user_input]
 # print(new)
 
 # Method-2
-output_list = [phonetic_dict[letter] for letter in list1]
-print(output_list)
+# output_list = [phonetic_dict[letter] for letter in list1]
+# print(output_list)
