6c95e2d25636d01542cd6d7dc9cb957da1eefa74
Manas Chagi <115474765+ManasChagi@users.noreply.github.com>
2024-05-14 00:02:56 +0530

    Update main.py
    NEW Features added

--- quiz game/main.py
+++ quiz game/main.py
@@ -1,17 +1,15 @@
 from question_model import Question
 from data import question_data
-from quiz_brain import Quizbrain
+from quiz_brain import QuizBrain
+from ui import QuizInterface
 
-qbank = []
-for each in question_data:
-    question_text = each["question"]
-    question_answer = each["correct_answer"]
-    newq = Question(question_text, question_answer)
-    qbank.append(newq)
+question_bank = []
+for question in question_data:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
 
-quiz = Quizbrain(qbank)
-while quiz.still_has_questions():
-    quiz.next_question()
-print("You've completed your quiz")
-print(f"your final score is {quiz.score}/{quiz.qno}")
 
+quiz = QuizBrain(question_bank)
+quiz_ui = QuizInterface(quiz)
